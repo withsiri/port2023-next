f4e90659a101adf81edea8d0985d2730493115df
m2 <withsiri91@gmail.com>
2023-09-13 00:07:38 +0900

    사이트 완료

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { siteText } from "@/constants";
 
 const Site = () => {
-  return (
-    <div>Site</div>
-  )
+	return (
+		<section id="site">
+			<div className="site_inner">
+				<h2 className="site_title">site coding <em>나의 작업물</em></h2>
+				<div className="site_wrap">
+					{siteText.map((site, key) => (
+						<article className={`site_item s${key+1}`} key={key}>
+							<span className="num">{key+1}.</span>
+							<div className="text">
+								<div>{site.text[0]}</div>
+								<div>{site.text[1]}</div>
+								<div>{site.text[2]}</div>
+							</div>
+							<h3 className="title">{site.title}</h3>
+							<div className="btn">
+								<a href={site.code}>code</a>
+								<a href={site.view}>site</a>
+							</div>
+							<div className="info">
+								<span>{site.info[0]}</span>
+								<span>{site.info[1]}</span>
+								<span>{site.info[2]}</span>
+							</div>
+						</article>			
+					))}
+				</div>
+			</div>
+		</section>
+	)
 }
 
-export default Site
+export default Site;
