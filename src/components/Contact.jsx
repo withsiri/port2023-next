f84a3c7463725ec9f260dc2a63aad857529c7855
m2 <withsiri91@gmail.com>
2023-09-13 10:39:31 +0900

    연락처완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { contactText } from "@/constants";
 
 const contact = () => {
-  return (
-    <div>contact</div>
-  )
+	return (
+		<section id="contact">
+			<div className="contact_inner">
+				<h2 className="contact_title">contact</h2>
+				<div className="contact_lines top" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+				<div className="contact_text">
+					<div className="text">
+						{contactText.map((contact, key) => (
+							<div>
+								<a href={contact.link} target="_blank">{contact.title}</a>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="contact_lines" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+			</div>
+		</section>
+  	)
 }
 
-export default contact
+export default contact;
