863fcd1ce187e6ad5d89af814b104116ad78b020
m2 <withsiri91@gmail.com>
2023-09-15 10:13:56 +0900

    푸터 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { footerText } from "@/constants";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer id="footer" role="contentinfo">
+			<div className="footer_inner">
+				<h2 className="footer_text">
+					<div>withsiri91</div>
+					<div>@siri</div>
+				</h2>
+				<div className="footer_info">
+					<div className="left">
+						<div className="title"><a href="#">sign up</a></div>
+						<p className="desc">회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+					</div>
+					<div className="right">
+						<h3>social</h3>
+						<ul>
+							{footerText.map((footer, key) => (
+								<li key={key}>
+									<a href={footer.link} target="_blank" rel="nopener noreferrer">{footer.title}</a>
+									<em>{footer.desc}</em>
+								</li>
+							))}
+						</ul>
+					</div>
+
+				</div>
+				<div className="footer_bottom">
+					&copy;2023 withsiri91<br />
+					이 사이트는 비트를 이용하여 제작하였습니다.
+				</div>
+			</div>
+		</footer>
+  	)
 }
 
-export default Footer
+export default Footer;
